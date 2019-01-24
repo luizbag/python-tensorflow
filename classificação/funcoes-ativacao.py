36dddfe56d37230d4db1cd99a0af80740779ac44
Luiz Gavioli <428691+luizbag@users.noreply.github.com>
2019-01-24 19:05:42 -0200

    Classificação

---
+++ classificação/funcoes-ativacao.py
@@ -0,0 +1,33 @@
+import numpy as np
+# transfer function
+
+def stepFunction(soma):
+    if (soma >= 1):
+        return 1
+    return 0
+
+def sigmoidFunction(soma):
+    return 1 / (1 + np.exp(-soma))
+
+def tahnFunction(soma):
+    return (np.exp(soma) - np.exp(-soma)) / (np.exp(soma) + np.exp(-soma))
+
+def reluFunction(soma):
+    if soma >= 0:
+        return soma
+    return 0
+
+def linearFunction(soma):
+    return soma
+
+def softmaxFunction(x):
+    ex = np.exp(x)
+    return ex / ex.sum()
+
+teste = stepFunction(-1)
+teste = sigmoidFunction(-0.358)
+teste = tahnFunction(-0.358)
+teste = reluFunction(0.358)
+teste = linearFunction(-0.358)
+valores = [7.0, 2.0, 1.3]
+print(softmaxFunction(valores))
